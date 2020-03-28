bb4a25f5417709b7691c2e3c1b096afd5861951d
Pranav Mathur <pranavmathur001@gmail.com>
2020-03-28 14:53:07 -0400

    Created Request for Typform responses, currently testing

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const embedElement = document.getElementById("typeform_container");
-typeformEmbed.makeWidget(
-    embedElement,
-    'https://pranavmathur001.typeform.com/to/LO0U0p'
-);
+// typeform auth token: 5evke2jp3XoYUgdSuMgpVdKpovpweVBdPqTrZRiyB5To
+
+$(document).ready(function() {
+    const embedElement = document.getElementById("form_map");
+    typeformEmbed.makeWidget(
+        embedElement,
+        'https://pranavmathur001.typeform.com/to/LO0U0p',
+        {
+            onSubmit: function() {
+
+            }
+        }
+    );
+
+    const typeform_url = 'https://api.typeform.com/forms/LO0U0p/responses'
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readystate == 4 && this.status == 200) {
+            console.log(xhttp.responseText);
+        }
+    };
+
+    xhttp.open("GET", typeform_url);
+    xhttp.setRequestHeader("Authorization", "Bearer 5evke2jp3XoYUgdSuMgpVdKpovpweVBdPqTrZRiyB5To");
+    xhttp.send(null);
+});
