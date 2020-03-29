88c273ba37da26250de5f770780cf8d60f688104
Pranav Mathur <pranavmathur001@gmail.com>
2020-03-29 16:28:26 -0400

    Added hospital validation and started styling the form

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 currentPage = 0;
 
 $(function() {
-    showPage(0);
+    showPage(0); // first load the patient/hospital selection
     $('#patient_button').on('click', function() {
-        currentPage = 1;
+        currentPage = 1; // go to the patient form
         showPage();
     });
 
     $('#hospital_button').on('click', function() {
-        currentPage = 3;
+        currentPage = 3; // go to the hospital form
         showPage();
     });
 
@@ -24,6 +24,7 @@ $(function() {
         showPage();
     });
 
+    // validate form before you move on to the next page
     $('#next_button').on('click', function() {
         if (currentPage === 1)
         {
@@ -38,35 +39,67 @@ $(function() {
             }
             else
             {
-
+                currentPage = currentPage + 1;
+                showPage();
             }
         }
         else if (currentPage === 2)
         {
-            patientSubmit();
+            patientName = $("#patient_name").val();
+            patientAge = $("#age").val();
+            patientAddress = $("#address").val();
+            transportation = $("input[name='transportation']:checked").val();
+
+            let weights = [0.879, 0.677, 0.381, 0.334, 0.186, 0.139, 0.114, 0.05, 0.048, 0.037]
+            symptomsFields = $("input[name='is_corona']");
+            symptoms = 0;
+            for (let i = 0; i < weights.length; i++)
+            {
+                symptoms += symptomsFields[i].checked ? weights[i] : 0;
+            }
+            symptoms = (symptoms / 2.845) * 10;
+
+            prevCondFields = $("input[name='pre_conditions']");
+            prev_conditions = 0;
+            for (let i = 0; i < prevCondFields.length; i++)
+            {
+                prev_conditions += prevCondFields[i].checked ? 1 : 0;
+            }
+
+            patientData = {
+                "age": patientAge,
+                "transporation": transporation,
+                "symptoms": symptoms,
+                "prev_conditions": prev_conditions
+            };
+
         }
         else if (currentPage === 3)
         {
             hospitalId = $("#hospital_id").val();
             const toSend =
             {
-                hospital_id: hospitalId
+                "hospital_id": hospitalId
             };
 
-            xhr = new XMLHttpRequest();
-            xhr.onload = function() {
-                if (xhr.status === 200)
+            $.post("https://wb4tf07f30.execute-api.us-east-1.amazonaws.com/prod/validate-hospital", JSON.stringify(toSend), function(data, status) {
+                if (status === "success")
                 {
-                    console.log("FUCK YEAH");
+                    if (data["body"] === "\"Found\"")
+                    {
+                        currentPage++;
+                        showPage();
+                    }
+                    else
+                    {
+                        alert("Hospital ID invalid")
+                    }
                 }
-            };
-
-            xhr.open("POST", "https://wb4tf07f30.execute-api.us-east-1.amazonaws.com/prod");
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(JSON.stringify(toSend));
+            });
         }
         else if (currentPage == 4)
         {
+            hospital_id = $("hospital_id").val();
             capacity = $("#capacity").val();
             beds = $("#beds").val();
             icus = $("#icu").val();
@@ -74,25 +107,30 @@ $(function() {
             tests = $("#tests").val();
             covidPatients = $("#covid_patients").val();
 
-            hospitalSubmit();
+            hospitalData = {
+                "hospital_id": hospital_id,
+                "capacity": capacity,
+                "beds": beds,
+                "icus": icus,
+                "ventilators": ventilators,
+                "tests": tests,
+                "covid_patients": covidPatients
+            };
+
+            $.post("https://wb4tf07f30.execute-api.us-east-1.amazonaws.com/prod/update", JSON.stringify(hospitalData), function(data, status) {
+                if (status === "success")
+                {
+                    alert("Thanks for submitting! Your data helps raise the hospital capacity line accross the country");
+                }
+            });
         }
         else
         {
-            currentPage = currentPage + 1;
+            currentPage++;
             showPage();
         }
     })
 });
-
-function patientSubmit()
-{
-
-}
-
-function hospitalSubmit()
-{
-
-}
 
 function showPage()
 {
