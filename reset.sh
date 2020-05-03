315858c3ed84398575bf45a8ba7f52717a84279d
Ron Nachum <ron@ip-172-31-37-176.ec2.internal>
2020-05-03 04:23:54 +0000

    Server-side Updates

--- reset.sh
+++ reset.sh
@@ -4,8 +4,11 @@ cd /home/ubuntu/hospifind
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" |xargs rm -rf
 sudo pkill -f uwsgi -9
 sudo pkill -f nginx -9
+sudo systemctl daemon-reload
 sudo systemctl restart uwsgi
 sudo systemctl restart nginx
+sudo systemctl enable uwsgi
+sudo systemctl enable nginx
 sudo systemctl restart hospifind
 sudo systemctl start hospifind
 sudo systemctl enable hospifind
