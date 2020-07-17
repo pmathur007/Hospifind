531e77d1066e02532257e1c38634e361541e4d2c
Ron <gokepler@gmail.com>
2020-07-17 10:38:04 -0400

    Fixed Bars on Hospital Cards

--- application/florida_data_extraction.py
+++ application/florida_data_extraction.py
@@ -1,13 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 beds = "https://bi.ahca.myflorida.com/t/ABICC/views/Public/HospitalBedsHospital"
 icus = "https://bi.ahca.myflorida.com/t/ABICC/views/Public/ICUBedsHospital"
 
-beds_page = requests.get(beds)
-icus_page = requests.get(icus)
+# beds_page = requests.get(beds)
+# icus_page = requests.get(icus)
+# beds_soup = BeautifulSoup(beds_page.content, 'html.parser')
+# print(beds_soup)
 
-beds_soup = BeautifulSoup(beds_page.content, 'html.parser')
-
-print(beds_soup)
-
+session = HTMLSession()
+beds_resp = session.get(beds)
+beds_resp.html.render()
+soup = BeautifulSoup(beds_resp.html.html, 'html.parser')
+print(soup)
